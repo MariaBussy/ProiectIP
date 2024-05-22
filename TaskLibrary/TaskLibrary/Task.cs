8a659c4dda47334e59e88a1dddaa14d766341c5b
Maria Busaga <maria.busaga@student.tuiasi.ro>
2024-05-22 19:16:34 +0300

    dll-ul cu structura pentru task si testele unitare pentru aceasta

---
+++ TaskLibrary/TaskLibrary/Task.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskLibrary
+{
+    public class Task
+    {
+        private String numeTask;
+        private DateTime dataAsignarii;
+        private double oreLogate;
+        private String descriereTask;
+        private String numeAssigner;
+
+        public Task(in String nume, in String numePersoana)
+        {
+            if (nume == null || nume == "" || nume.Length < 5)
+            {
+                throw new Exception("Invalid task name. -> Task name has at least 5 letters.");
+            }
+            else
+            {
+                numeTask = nume;
+            }
+
+            if (numePersoana == null || numePersoana == "" || numePersoana.Length < 5)
+            {
+                throw new Exception("Invalid assigner name. -> Assigner name has at least 5 letters.");
+            }
+            else
+            {
+                numeAssigner = numePersoana;
+            }
+
+            dataAsignarii = DateTime.Now;
+
+            descriereTask = "";
+            oreLogate = 0.0;
+        }
+
+        public string NumeTask
+        {
+            get { return numeTask; }
+            set {
+                if (value == null || value == "" || value.Length<5)
+                {
+                    throw new Exception("Invalid task name. -> Task name has at least 5 letters.");
+                }
+                else
+                {
+                    numeTask = value;
+                }
+            }
+        }
+
+        public String DataAsignarii
+        {
+            get { return dataAsignarii.ToString("dd-MM-yyyy"); }
+        }
+
+        public double OreLogate
+        {
+            get { return oreLogate; }
+            set
+            {
+                if (value <= 0.0)
+                {
+                    throw new Exception("Invalid value for logged hours.-> value>0");
+                }
+                else
+                {
+                    oreLogate = value;
+                }
+            }
+        }
+
+        public String DescriereTask
+        {
+            get { return descriereTask; }
+            set { descriereTask = value; }
+        }
+
+        public String NumeAssigner
+        {
+            get { return numeAssigner; }
+            set {
+                if (value == null || value == "" || value.Length < 5)
+                {
+                    throw new Exception("Invalid assigner name. -> Assigner name has at least 5 letters.");
+                }
+                else
+                {
+                    numeAssigner = value;
+                }
+            }
+        }
+    }
+}
---
+++ TaskLibrary/TaskUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TaskUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TaskUnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bbd062ac-343e-43ca-85be-a1dd7b02bdff")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
